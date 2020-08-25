426bf948264fe60c7b15846c0bab5d7c28ebeed4
Leandro Oliveira <lfoadm@icloud.com>
2020-08-25 15:07:20 -0300

    Creating Migration, Model, Controller, Factories

---
+++ database/factories/FishFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Fish;
+use Faker\Generator as Faker;
+
+$factory->define(Fish::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
