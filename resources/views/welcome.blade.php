27af60d11c0c230f262065d6c795ca311e764262
Leandro Oliveira <lfoadm@icloud.com>
2020-08-29 08:09:48 -0300

    JWT Package

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,14 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #009688;
-                color: #005008;
+                background-color: #F1F6FB;
+                color: rgb(22, 21, 21);
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
@@ -49,10 +51,21 @@
             }
 
             .toba {
-                color: #fff;
+                color: #f15f47; 
                 font-size: 50px;
             }
+            .form-group {
+                font-size: 25px;
+                color: rgb(22, 21, 21);
 
+            }
+            .complexo {
+                font-size: 20px;
+                color: rgb(22, 21, 21);
+
+            }
+
+            
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -62,6 +75,7 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
+            
 
             .m-b-md {
                 margin-bottom: 30px;
@@ -92,9 +106,34 @@
                 <div class="toba">
                     <p>Sua pesca on line</p>
                 </div>
-                
+<hr>
+
+                <div class="leandro">
+                    <form>
+                        <div class="form-group">
+                          <label for="exampleInputEmail1">E-mail de Acesso</label>
+                          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="digite seu e-mail">
+                          <div  class="complexo">
+                            <small id="emailHelp" class="form-text text-muted">Nunca compartilharemos seu e-mail com mais ninguém.</small>
+                          </div>
+                          
+                        </div>
+                        <div class="form-group">
+                          <label for="exampleInputPassword1">Senha</label>
+                          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+                        </div>
+                        <div class="form-check">
+                          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                          <label class="form-check-label" for="exampleCheck1">Lembrar senha</label>
+                        </div>
+                        <button type="submit" class="btn btn-success">Entrar</button>
+                      </form>                
+                </div>
                 
             </div>
         </div>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </body>
 </html>
